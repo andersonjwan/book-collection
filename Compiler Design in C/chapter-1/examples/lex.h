77562cb0e2e55e176d45dac872369ef8e114167c
andersonjwan <andersonjwan@outlook.com>
2020-08-04 13:56:57 -0700

    added spacing

--- Compiler Design in C/chapter-1/examples/lex.h
+++ Compiler Design in C/chapter-1/examples/lex.h
@@ -1,12 +1,12 @@
 #ifndef LEX_H
 #define LEX_H
 
-#define EOI    0     /*  end of input   */
-#define SEMI   1     /*     ;           */
-#define PLUS   2     /*     +           */
-#define TIMES  3     /*     *           */
-#define LPAREN 4     /*     (           */
-#define RPAREN 5     /*     )           */
-#define NUM_ID 6     /* [0-9]* or [Aa-Zz]* */
+#define EOI       0     /*  end of input      */
+#define SEMI      1     /*     ;              */
+#define PLUS      2     /*     +              */
+#define TIMES     3     /*     *              */
+#define LPAREN    4     /*     (              */
+#define RPAREN    5     /*     )              */
+#define NUM_ID    6     /* [0-9]* or [Aa-Zz]* */
 
 #endif
