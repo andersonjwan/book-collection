35a0e80a6d10c47766adff91c22e0fc2d85b25b0
andersonjwan <andersonjwan@outlook.com>
2020-08-04 23:01:54 -0700

    included forward declarations

---
+++ Compiler Design in C/chapter-1/examples/parser.h
@@ -0,0 +1,12 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+/* forward declarations */
+void statements(void);
+void expression(void);
+void expression_prime(void);
+void term(void);
+void term_prime(void);
+void factor(void);
+
+#endif
