1c9c51ba905b6cfd4435408a3fd9cfbea36dffe2
andersonjwan <andersonjwan@outlook.com>
2020-08-05 13:53:24 -0700

    implemented intermediate code generation with tempory variables

---
+++ Compiler Design in C/chapter-1/examples/retval.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include "name.h"
+#include "retval.h"
+#include "lex.h"
+
+void
+statements(void)
+{
+  char *tmpvar;
+
+  while(!match(EOI)) {
+    tmpvar = expression();
+
+    if(match(SEMI)) {
+      advance();
+    }
+    else {
+      fprintf(stderr, "%d: Expected semicolon\n", yylineno);
+    }
+  }
+}
+
+char *
+expression(void)
+{
+  char *tmpvar_01, *tmpvar_02;
+
+  tmpvar_01 = term();
+
+  while(match(PLUS)) {
+    advance();
+    tmpvar_02 = term();
+
+    printf("    %s += %s\n", tmpvar_01, tmpvar_02);
+    free_name(tmpvar_02);
+  }
+
+  return tmpvar_01;
+}
+
+char *
+term(void)
+{
+  char *tmpvar_01, *tmpvar_02;
+
+  tmpvar_01 = factor();
+
+  while(match(TIMES)) {
+    advance();
+    tmpvar_02 = factor();
+
+    printf("    %s *= %s\n", tmpvar_01, tmpvar_02);
+    free_name(tmpvar_02);
+  }
+
+  return tmpvar_01;
+}
+
+char *
+factor(void)
+{
+  char *tmpvar;
+
+  if(match(NUM_ID)) {
+    printf("    %s = %0.*s\n", tmpvar = new_name(), yylen, yytext);
+    advance();
+  }
+  else if(match(LPAREN)) {
+    advance();
+    tmpvar = expression();
+
+    if(match(RPAREN)) {
+      advance();
+    }
+    else {
+      fprintf(stderr, "%d: Mismatched parenthesis\n", yylineno);
+    }
+  }
+  else {
+    fprintf(stderr, "%d: Number of Identifier expected.\n", yylineno);
+  }
+
+  return tmpvar;
+}
---
+++ Compiler Design in C/chapter-1/examples/retval.h
@@ -0,0 +1,10 @@
+#ifndef RETVAL_H
+#define RETVAL_H
+
+/* forward declarations */
+void statements(void);
+char * expression(void);
+char * term(void);
+char * factor(void);
+
+#endif
